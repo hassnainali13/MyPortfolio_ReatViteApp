4863ef4266e3921ae7dd5e1985937635b9ceedb2
HassnainAli <ahassnainsaleem964@gmail.com>
2026-01-05 22:25:18 +0500

    Fix case for Home.jsx and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import Navbar from "./Components/Navbar";
-import Home from "./Pages/home";
+
+
 
 const App = () => {
   return (
